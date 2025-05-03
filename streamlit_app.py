2770877039eb519318c535b33b1dcd2bc8bbf714
CodeCrafter50 <c.calero.c@gmail.com>
2025-05-03 16:36:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,6 +174,9 @@ def main():
     option = [nombre_modelo] 
     model = st.sidebar.selectbox('Modelo con la mejor precisión detectada', option)
 
+    st.subheader('Alumno: Carlos Calero') 
+    st.subheader('Curso: Talleres')
+    
     st.subheader('Parámetros de entrada del usuario') 
     st.subheader(model)
     st.write(df)
