9983420895486d627dc90ed21dbe4aa5546791c1
CodeCrafter50 <c.calero.c@gmail.com>
2025-05-03 16:39:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,8 +174,8 @@ def main():
     option = [nombre_modelo] 
     model = st.sidebar.selectbox('Modelo con la mejor precisión detectada', option)
 
-    st.markdown("**Alumno: Carlos Calero**")
-    st.markdown("**Curso: Talleres**")
+    st.markdown("### **Alumno: Carlos Calero**")
+    st.markdown("### **Curso: Talleres**")
     
     st.subheader('Parámetros de entrada del usuario') 
     st.subheader(model)
